fb224865388a38c81c5ccc0f83ec003531e1941e
ANDREW SEGAL <segal.a@hotmail.com>
2019-12-03 13:55:16 -0500

    Messing around with routes

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const andrew = { name: "Andrew", age: 28, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(andrew);
+  res.json(req.query)
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
