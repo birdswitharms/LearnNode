d94103e3215aa7953d075d914fdfe28577b72b38
ANDREW SEGAL <segal.a@hotmail.com>
2019-12-04 15:19:22 -0500

    Add updating stores and implement lat/lng map data

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -2,16 +2,41 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  res.render('index');
+  res.render('index', { title: 'Main' });
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store'});
+  res.render('editStore', { title: 'Add Store' });
 };
 
 exports.createStore = async (req, res) => {
-  const store = new Store(req.body);
-  await store.save()
-  res.redirect('/');
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // find store
+  const store = await Store.findOne({ _id: req.params.id });
+  // render edit form with store info
+  res.render('editStore', { title: `Edit ${store.name}`, store});
+}
+
+exports.updateStore = async (req, res) => {
+  // set the location data to be a point
+  req.body.location.type = 'Point';
+  // find and update a stre
+  // const store = Store.findOneAndUpdate(query, data, options)
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec()
+  // redirect to store and flash success
+  req.flash('success', `Successfuly updated ${store.name}. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+}
